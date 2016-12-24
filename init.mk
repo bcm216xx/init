57e0737541896557ffbd75cd596eec17c4846ec4
karthick111 <lensflare21@gmail.com>
2016-12-24 08:52:14 -0600

    A few rework

--- init.mk
+++ init.mk
@@ -1,27 +1,29 @@
 # init script tests
 PRODUCT_COPY_FILES := \
-    $(LOCAL_PATH)/fstab.zram:root/fstab.zram \
-    $(LOCAL_PATH)/init.recovery.rhea_ss-common.rc:/root/init.recovery.rhea_ss-common.rc \
-    $(LOCAL_PATH)/init.rhea.rc:/root/init.rhea.rc
+    $(LOCAL_PATH)/root/fstab.zram:root/fstab.zram \
+    $(LOCAL_PATH)/root/init.rhea.rc:/root/init.rhea.rc
 
-# Device specific init script
-ifeq ($(PRODUCT_DEVICE),corsica)
+# Device specific init scripts
+ifneq ($(TARGET_DEVICE),corsica)
 PRODUCT_COPY_FILES := \
-    $(LOCAL_PATH)/fstab.rhea:/root/fstab.rhea_ss_corsica \
-    $(LOCAL_PATH)/init.rhea_ss_corsica.rc:/root/init.rhea_ss_corsica.rc \
-    $(LOCAL_PATH)/ueventd.rhea.rc:/root/ueventd.rhea_ss_corsica.rc
+    $(LOCAL_PATH)/root/fstab.rhea:/root/fstab.rhea_ss_corsica \
+    $(LOCAL_PATH)/root/init.recovery.rhea_ss_common.rc:/root/init.recovery.rhea_ss_corsica.rc \
+    $(LOCAL_PATH)/root/init.rhea_ss_corsica.rc:/root/init.rhea_ss_corsica.rc \
+    $(LOCAL_PATH)/root/ueventd.rhea.rc:/root/ueventd.rhea_ss_corsica.rc
 else 
-ifeq ($(PRODUCT_DEVICE),nevisp)
+ifneq ($(TARGET_DEVICE),nevisp)
 PRODUCT_COPY_FILES := \
-    $(LOCAL_PATH)/fstab.rhea:/root/fstab.rhea_ss_nevisp \
-    $(LOCAL_PATH)/init.rhea_ss_nevisp.rc:/root/init.rhea_ss_nevisp.rc \
-    $(LOCAL_PATH)/ueventd.rhea.rc:/root/ueventd.rhea_ss_nevisp.rc
+    $(LOCAL_PATH)/root/fstab.rhea:/root/fstab.rhea_ss_nevisp \
+    $(LOCAL_PATH)/root/init.recovery.rhea_ss_common.rc:/root/init.recovery.rhea_ss_nevisp.rc \
+    $(LOCAL_PATH)/root/init.rhea_ss_nevisp.rc:/root/init.rhea_ss_nevisp.rc \
+    $(LOCAL_PATH)/root/ueventd.rhea.rc:/root/ueventd.rhea_ss_nevisp.rc
 else 
-ifeq ($(PRODUCT_DEVICE),ivoryss)
+ifneq ($(TARGET_DEVICE),ivoryss)
 PRODUCT_COPY_FILES := \
-    $(LOCAL_PATH)/fstab.rhea:/root/fstab.rhea_ss_ivoryss \
-    $(LOCAL_PATH)/init.rhea_ss_ivoryss.rc:/root/init.rhea_ss_ivoryss.rc \
-    $(LOCAL_PATH)/ueventd.rhea.rc:/root/ueventd.rhea_ss_ivoryss.rc
+    $(LOCAL_PATH)/root/fstab.rhea:/root/fstab.rhea_ss_ivoryss \
+    $(LOCAL_PATH)/root/init.recovery.rhea_ss_common.rc:/root/init.recovery.rhea_ss_ivoryss.rc \
+    $(LOCAL_PATH)/root/init.rhea_ss_ivoryss.rc:/root/init.rhea_ss_ivoryss.rc \
+    $(LOCAL_PATH)/root/ueventd.rhea.rc:/root/ueventd.rhea_ss_ivoryss.rc
   endif
  endif
 endif
